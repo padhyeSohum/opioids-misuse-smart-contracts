7a0b0fa9ca28bcf045c12eb45cefdbc5f9dede9d
padhyeSohum <sohum.padhye@gmail.com>
2022-02-05 12:31:52 -0500

    completed the functions for checking if a person has a prescription and adding a prescription to a person if they don't already have it

--- contracts/MainContract.sol
+++ contracts/MainContract.sol
@@ -13,22 +13,35 @@ contract MainContract {
     }
 
     // 1. Create mapping for each address
-
     mapping(address => bool) hasPrescription;
 
     // 2. Check if account address has prescription
-    function checkIfHasPrescription() public view returns (bool _hasPrescription) {
-        if (hasPrescription[msg.sender]) {
-            return _hasPrescription;
+    function personHasPrescription(address accountAddress) public view returns (bool) {
+
+        accountAddress = msg.sender;
+        if (hasPrescription[accountAddress]) {
+            return true;
         }
         else {
-            return !_hasPrescription;
+            return false;
         }
+    }
+
+    // 3. If person does not have prescription, give them a prescription (pharmacist/provider of opioids will do this)
+    function givePrescription(address accountAddress) public returns (bool) {
+
+        accountAddress = msg.sender;
+
+        require (!hasPrescription[accountAddress], "this person already has a prescription");
+        
+        hasPrescription[accountAddress] = true;
+        return hasPrescription[accountAddress];
     }
 
 
     // Create mapping for each type of opioid to the amount of days it should take for the person to use up all of it
     // => keep track of time by taking the timestamp from which it was originally prescribed
+
     // Update
     // Add patient verification
 
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,53 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+// describe("Greeter", function () {
+//   it("Should return the new greeting once it's changed", async function () {
+//     const Greeter = await ethers.getContractFactory("Greeter");
+//     const greeter = await Greeter.deploy("Hello, world!");
+//     await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+//     expect(await greeter.greet()).to.equal("Hello, world!");
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+//     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+//     // wait until the transaction is mined
+//     await setGreetingTx.wait();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+//     expect(await greeter.greet()).to.equal("Hola, mundo!");
+//   });
+// });
+
+
+
+// describe("Opioids Misuse", function () {
+//     it("should do something", async function () {
+//         const MainContract = await ethers.getContractFactory("MainContract");
+//         const mainContract = await MainContract.deploy();
+//         await mainContract.deployed();
+
+//         expect(await mainContract.personHasPrescription('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266')).to.equal(false);
+
+//         const setHasPrescriptionTx = await mainContract.givePrescription('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+
+//         await setHasPrescriptionTx.wait();
+
+//         expect(await mainContract.personHasPrescription()).to.equal(true);
+//     });
+// })
+
+
+
+describe("Checking if person has prescription", function() {
+    it("should check if a person has a prescription", async function () {
+        const MainContract = await ethers.getContractFactory("MainContract");
+        const mainContract = await MainContract.deploy();
+        await mainContract.deployed();
+        
+        expect(await mainContract.personHasPrescription('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266')).to.equal(false);
+
+        await mainContract.givePrescription('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+
+        expect(await mainContract.personHasPrescription('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266')).to.equal(true);
+    })
+})
